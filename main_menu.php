399bda46c7fdf29f9e9ce019fbf59aa3aacc4b22
102100517 <102100517@student.swin.edu.au>
2019-10-04 00:08:16 +1000

    yes

---
+++ main_menu.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<!DOCTYPE html>";
+    echo "<html>";
+    echo "<head>"
+    echo "<title>THICCUMS</title>";
+    echo "</head>"
+    echo "<body>"
+    echo "<h1>THICCUMS</h1>";
+    echo "<p>yum</p>";
+    echo "</body>"
+    echo "</html>"
+?>
