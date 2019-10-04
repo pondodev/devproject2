6379704076ef742e77e74236232b0be44bf4fb2f
102100517 <102100517@student.swin.edu.au>
2019-10-04 19:19:53 +1000

    editing add_products_process

--- add_products.php
+++ add_products.php
@@ -1,10 +1,3 @@
-<?php
-    //error logging
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL);
-    include "dbconnect.php"
-?>
 <!DOCTYPE html>
     <html lang="en">
         <head>
--- add_products_process.php
+++ add_products_process.php
@@ -1,10 +1,15 @@
 <?php
-    if(issset($_POST["product_code"]) && isset($_POST["name"]) && isset($_POST["date_added"]) && isset($_POST["quantity_on_hand"]) && isset($_POST["price"]) && isset($_POST["comment"]))
+    //error logging
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors',1);
+    error_reporting(E_ALL);
+    include "dbconnect.php";
+    if(isset($_POST["product_code"]) && isset($_POST["name"]) && isset($_POST["date_added"]) && isset($_POST["quantity_on_hand"]) && isset($_POST["price"]) && isset($_POST["comment"]))
         Process($_POST["product_code"],$_POST["name"],$_POST["date_added"],$_POST["quantity_on_hand"],$_POST["price"],$_POST["comment"]);
 
     function Process($product_code,$name,$date_added,$quantity_on_hand,$price,$comment)
     {
-        include "dbconnect.php";
+        
         $query = "INSERT INTO 'products' (product_code,name,date_added,quantity_on_hand,price,comment) VALUES
         ('$product_code','$name','$date_added','$date_added','$quantity_on_hand','$price','$comment')
         ";
