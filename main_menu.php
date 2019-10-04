04877650fa0e83ba96c4678dde720ca94a9f2867
102100517 <102100517@student.swin.edu.au>
2019-10-04 10:44:09 +1000

    merged to single echo

--- main_menu.php
+++ main_menu.php
@@ -1,12 +1,12 @@
 <?php
-    echo "<!DOCTYPE html>";
-    echo "<html>";
-    echo "<head>"
-    echo "<title>THICCUMS</title>";
-    echo "</head>"
-    echo "<body>"
-    echo "<h1>THICCUMS</h1>";
-    echo "<p>yum</p>";
-    echo "</body>"
-    echo "</html>"
+    echo "<!DOCTYPE html>"
+         "<html>"
+            "<head>"
+                "<title>THICCUMS</title>"
+            "</head>"
+            "<body>"
+                "<h1>THICCUMS</h1>"
+                "<p>yum</p>"
+            "</body>"
+         "</html>";
 ?>
