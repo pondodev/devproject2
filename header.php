a0dedd76c5e65b139661d94c05d6495df7212088
Daniel Coady <daniel.coady@outlook.com.au>
2019-10-06 18:02:55 +1100

    did a bit of cleanup and refactoring

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav class="navbar navbar-expand navbar-dark bg-dark sticky-top">
+        <a href="main_menu.php" class="navbar-brand">PHP SRePS</a>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="add_products.php" class="nav-link">Add Products</a></li>
+            <li class="nav-item"><a href="show_products.php" class="nav-link">Show Products</a></li>
+            <li class="nav-item"><a href="edit_products.php" class="nav-link">Edit Products</a></li>
+        </ul>
+    </nav>
+</header>
