795a1414b6b01ea2303bd392f577fd4a130165e7
Daniel Coady <daniel.coady@outlook.com.au>
2019-10-09 21:09:46 +1100

    added show products page

---
+++ show_products.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Show All Products</title>
+</head>
+
+<body>
+    <?php include "header.php"; ?>
+    <h1>Show All Products</h1>
+    <?php include "show_products_process.php" ?>
+</body>
+</html>
