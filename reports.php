244a172278babe8d7567d37ea6a9f7cb1f96d0fc
Daniel Coady <daniel.coady@outlook.com.au>
2019-10-24 23:04:34 +1100

    added bootstrap to reports page

--- reports.php
+++ reports.php
@@ -2,6 +2,12 @@
 <html>
 <head>
     <title>Show Reports</title>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="author" content="James Hassall, Daniel Coady, Jake Beltrami, Gareth Clauson">
+            <meta name="description" content="Sales reporting software">
+            <meta name="keywords" content="Sales,Reporting,Software">
 </head>
 
 <body>
